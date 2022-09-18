aa9890561db7212de432f8d7c3e5b95b05e1c59d
katerina <you-like-me@yandex.ru>
2022-09-18 13:49:01 +0300

    Add Task

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[10] {"123", "23", "hello", "world", "res", ":))", "help", "gitignore", 
+"0249", "!++!"};
+string[] array2 = new string[array1.Length];
+void StringMore(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+StringMore(array1, array2);
+PrintArray(array2);
